03818fa734347dc4fc88c66be145a7daf02c7549
Ernestas <ernestas.veciorka@gmail.com>
2022-09-12 06:53:32 +0300

    modified blog views

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from .models import Blog
 
 def all_blogs(request):
 
-    blogs = Blog.objects.order_by('-date') # grabs all objects from database
+    blogs = Blog.objects.order_by('-date')[:5] # grabs all objects from database
 
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
